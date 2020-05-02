f51a765fd18e490f012576738dffd57c0a3e199e
Artur Kuznetsou <arturkuznetsou@pm.me>
2020-05-02 17:18:24 +0200

    worked on mergesort

--- algs/mergesort.js
+++ algs/mergesort.js
@@ -1,18 +1,19 @@
 async function merge(start, mid, end)
 {
-	var moved = 0;
-	var midb = mid;
-
-	while(start < mid && mid < indend)
+	var startold = start;
+	var ind = mid;
+	while(start < mid && ind < end)
 	{
-		if(greater(start, end))
+		if(greater(start, ind))
 		{
-			move(end, start + moved);
+			move(ind, start);
+			ind += 1;
+			mid += 1;
 		}
 		else{
-			move(start, start + moved);
+			move(start, start);
 		}
-		moved += 1;
+		start += 1;
 	}
 }
 
@@ -23,8 +24,13 @@ async function mergeSort()
 	busy = true;
 	var len = document.getElementById('nBars').value;
 	pairs = [];
-	for(var n = 0; n < len / 2; n++)
+	for(var n = 0; n < (len - (len % 2)) / 2; n++)
 	{
 		pairs.push([n * 2, n * 2 + 1]);
+	}
+	if(len % 2 == 1) { pairs[Math.floor(len / 2 - 1)][1] = len - 1; }
+	for(pair of pairs)
+	{
+		merge(pair[0], pair[1], pair[1] + 1);
 	}
 }
